b0adf9e10e9bce742e0fc23b6396e74ce5c7dd97
Rodrigo <rodrigo.garcia.torres04@gmail.com>
2023-06-21 11:04:19 -0600

    Agregue 3 algoritmos

---
+++ Rodrigo/fastPow.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// Exponenciacion recursiva
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+// exponenciacion iterativa
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+// aplicar una permutacion k veces
+vector<int> applyPermutation(vector<int> sequence, vector<int> permutation) {
+    vector<int> newSequence(sequence.size());
+    for(int i = 0; i < sequence.size(); i++) {
+        newSequence[i] = sequence[permutation[i]];
+    }
+    return newSequence;
+}
+
+vector<int> permute(vector<int> sequence, vector<int> permutation, long long b) {
+    while (b > 0) {
+        if (b & 1) {
+            sequence = applyPermutation(sequence, permutation);
+        }
+        permutation = applyPermutation(permutation, permutation);
+        b >>= 1;
+    }
+    return sequence;
+}
+
+int main() {
+
+}
