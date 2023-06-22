ef2823be4fa7fb85cd5badf5712d112b13514650
SergioGzzBrz <114702763+SergioGzzBrz@users.noreply.github.com>
2023-06-22 07:35:02 -0600

    Update Fast Pow.cpp Sergio

--- Sergio/Fast Pow.cpp
+++ Sergio/Fast Pow.cpp
@@ -8,20 +8,16 @@ typedef long long ll;
 const ll mod = 1e9+7;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--){
-        ll a, b;
-    	cin >> a >> b;
-    	ll res = 1;
-    	while (b > 0) {
-    	    if (b & 1){
-    	       res =  res * a % mod;
-    	    }
-    	    
-    	    a = a * a % mod;
-    	    b >>= 1;
-    	}
-    	cout << res << endl;
+    ll a, b;
+    cin >> a >> b;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1){
+           res =  res * a % mod;
+        }
+        
+        a = a * a % mod;
+        b >>= 1;
     }
+    cout << res << endl;
 }
